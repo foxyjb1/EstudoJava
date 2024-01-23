0f516c8da51074412ae6c9a575da2c154ddcf621
joshua_Bf <119230166+foxyjb1@users.noreply.github.com>
2024-01-22 23:49:52 -0300

    Add files via upload

---
+++ RevisaoJanelas/src/classes/Funcionario.java
@@ -0,0 +1,78 @@
+package classes;
+
+public class Funcionario {
+    //atributos
+    private int id;
+    private String nome;
+    private float salario;
+    private String email;
+    private String endereco;
+    private String telefone;
+    private int idade;
+    private String cargo; 
+    //metodos
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public float getSalario() {
+        return salario;
+    }
+
+    public void setSalario(float salario) {
+        this.salario = salario;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+    
+}
---
+++ RevisaoJanelas/src/revisaojanelas/RevisaoJanelas.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package revisaojanelas;
+
+import janelas.FrmPrincipal;
+
+/**
+ *
+ * @author jefersonsilva
+ */
+public class RevisaoJanelas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FrmPrincipal FP = new FrmPrincipal();
+        FP.setTitle("Sistema de Cadastros");
+        FP.setVisible(true);
+    }
+    
+}
