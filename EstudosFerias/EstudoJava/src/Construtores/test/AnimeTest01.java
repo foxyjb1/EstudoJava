f81481c1dd4537896109015962253202600682e7
joshua_Bf <barraganjfox@gmail.com>
2024-01-26 18:42:10 -0300

    Construtores

---
+++ EstudosFerias/EstudoJava/src/Construtores/test/AnimeTest01.java
@@ -0,0 +1,11 @@
+package Construtores.test;
+
+import Construtores.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("kimetsu no Yaiba","Streming",11,"ação");
+        anime.imprime();
+    }
+}
+
