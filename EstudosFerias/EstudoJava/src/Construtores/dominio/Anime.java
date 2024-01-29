5444d149484f52d77cc302d9534dac92a6b93533
joshua_Bf <barraganjfox@gmail.com>
2024-01-29 13:27:17 -0300

    construtores

--- EstudosFerias/EstudoJava/src/Construtores/dominio/Anime.java
+++ EstudosFerias/EstudoJava/src/Construtores/dominio/Anime.java
@@ -6,15 +6,25 @@ public class Anime {
     private int episodios;
     private String genero;
 
+    private  String estudio;
+
 
 
 
     public Anime(String nome,String tipo , int episodios, String genero){
-
+        this();
         this.nome = nome;
         this.tipo = tipo;
         this.episodios = episodios;
         this.genero = genero;
+    }
+    public Anime (String nome , String tipo , int episodios, String genero,String estudio){
+        this(nome ,tipo ,episodios , genero );
+        this.estudio = estudio;
+    }
+
+    public Anime(){
+        System.out.println("Dentro do Construtor sem argumentos");
     }
 
 //    public void init(String nome,String tipo ,int episodios){
--- EstudosFerias/EstudoJava/src/Construtores/test/AnimeTest01.java
+++ EstudosFerias/EstudoJava/src/Construtores/test/AnimeTest01.java
@@ -4,7 +4,7 @@ import Construtores.dominio.Anime;
 
 public class AnimeTest01 {
     public static void main(String[] args) {
-        Anime anime = new Anime("kimetsu no Yaiba","Streming",11,"ação");
+        Anime anime = new Anime("kimetsu no Yaiba","Streming",11,"ação","Mappa");
         anime.imprime();
     }
 }
